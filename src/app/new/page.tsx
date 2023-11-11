201768ffbf73db90c1d8f6941baaf3ecbc7504a1
Mirai-explorer <tangjw0226@gmail.com>
2023-11-12 03:25:38 +0800

    test

---
+++ src/app/new/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import dynamic from "next/dynamic";
+
+const New = dynamic(() => import('@/components/New'), {
+    ssr: false
+});
+
+const Page = () => {
+    return(
+        <New></New>
+    )
+}
+
+export default Page;
---
+++ src/components/New/index.tsx
@@ -0,0 +1,16 @@
+"use client"
+import React from 'react';
+import crypto from "crypto-js";
+
+const Barcode = () => {
+    const str = '1234637'
+    const strMd5 = crypto.MD5(str).toString().toLowerCase()
+    console.log(strMd5)
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Barcode;
